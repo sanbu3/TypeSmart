6d7209ab65dac5d821a01fa3df594ed87137a97e
王汪旺 <sanbu.wang@gmail.com>
2025-05-24 12:43:40 +0800

    Initial Commit

---
+++ InputSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InputSwitcher
+//
+//  Created by wang on 2025/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InputSwitcher/InputSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputSwitcherApp.swift
+//  InputSwitcher
+//
+//  Created by wang on 2025/5/24.
+//
+
+import SwiftUI
+
+@main
+struct InputSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
